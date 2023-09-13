ec1d5174a4c75b05ab1b56ed8757fb1b111d5b37
Duy Huynh <huynhtruongdyu@gmail.com>
2023-09-13 15:48:58 +0700

    wip

---
+++ src/Client/osbm-admin/src/app/demo/page.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+export default function Page() {
+  return <Fragment>
+    <section>
+      <h1 className="title">Typography</h1>
+      <h1>h1 - the lazy fox jumps over brown dog</h1>
+      <h2>h2 - the lazy fox jumps over brown dog</h2>
+      <h3>h3 - the lazy fox jumps over brown dog</h3>
+      <h4>h4 - the lazy fox jumps over brown dog</h4>
+      <h5>h5 - the lazy fox jumps over brown dog</h5>
+      <h6>h6 - the lazy fox jumps over brown dog</h6>
+    </section>
+    <section>
+      <h1 className="title">Typography</h1>
+      <button className="btn">Button</button>
+    </section>
+  </Fragment>
+}
